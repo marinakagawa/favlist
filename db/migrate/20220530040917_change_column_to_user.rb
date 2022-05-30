6f34158b6300a40c4ddf51240338d5a4c39fcbbc
MariNakagawa <n03mari@gmail.com>
2022-05-30 13:55:31 +0900

    add migration file

---
+++ db/migrate/20220530040917_change_column_to_user.rb
@@ -0,0 +1,27 @@
+class ChangeColumnToUser < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :user_name, :string, unique: true
+    add_column :users, :admin, :boolean, default: 0, null: false
+    ## Trackable
+    add_column :users, :sign_in_count, :integer, default: 0, null: false
+    add_column :users, :current_sign_in_at, :datetime
+    add_column :users, :last_sign_in_at, :datetime
+    add_column :users, :current_sign_in_ip, :string
+    add_column :users, :last_sign_in_ip, :string
+
+    ## Confirmable
+    add_column :users, :confirmation_token, :string
+    add_column :users, :confirmed_at, :datetime
+    add_column :users, :confirmation_sent_at, :datetime
+    add_column :users, :unconfirmed_email, :string # Only if using reconfirmable
+
+    ## Lockable
+    add_column :users,  :failed_attempts, :integer, default: 0, null: false # Only if lock strategy is :failed_attempts
+    add_column :users, :unlock_token, :string # Only if unlock strategy is :email or :both
+    add_column :users,  :locked_at, :datetime
+
+    add_index :users, :confirmation_token,   unique: true
+    add_index :users, :unlock_token,         unique: true
+    add_index :users, :admin
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_14_052753) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_30_040917) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -19,8 +19,25 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_14_052753) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_name"
+    t.boolean "admin", default: false, null: false
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
+    t.index ["admin"], name: "index_users_on_admin"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
 end
