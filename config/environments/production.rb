ebc3a11ed063f9bddd2e8735b9c7adbc0968c0cb
MariNakagawa <n03mari@gmail.com>
2022-06-08 13:31:40 +0900

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,7 @@ Rails.application.configure do
     :password => ENV['MAILGUN_SMTP_PASSWORD'],
     :domain => host,
     :authentication => :plain,
+    :enable_starttls_auto => true
   }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
