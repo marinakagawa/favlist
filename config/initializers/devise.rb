1977b98f614e2f9db83e29595aad3a967a1a1261
MariNakagawa <n03mari@gmail.com>
2022-06-27 14:12:43 +0900

    update

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -42,7 +42,7 @@ Devise.setup do |config|
   config.mailer_sender = 'favlist.info@gmail.com'
 
   # Configure the class responsible to send e-mails.
-  config.mailer = 'Users::Mailer'
+  config.mailer = 'Devise::Mailer'
 
   # Configure the parent class responsible to send e-mails.
   # config.parent_mailer = 'ActionMailer::Base'
