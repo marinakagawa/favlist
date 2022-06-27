a66fb032803719bcd898a4482d5344c186e2fdce
MariNakagawa <n03mari@gmail.com>
2022-06-27 14:39:27 +0900

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "untitled_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'fav-list.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'https://fav-list.herokuapp.com'}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
