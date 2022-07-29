6b037cd195334d61d6a6ac8137e3eaa49bd8e488
MariNakagawa <n03mari@gmail.com>
2022-07-29 12:26:34 +0900

    ログアウトのルーティング修正と、ユーザ名未設定時の表示の変更（未登録 => 未設定）

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -23,4 +23,20 @@ RSpec.describe "Users", type: :system do
     click_link "FavList"
     expect(page).to have_content "テスト"
   end
+
+  scenario "user logout" do
+    user = FactoryBot.create(:user)
+    visit root_path
+    click_link "ログイン"
+
+    fill_in "user[email]", with: user.email
+    fill_in "user[password]", with: user.password
+    click_button "ログイン"
+
+    click_on "ユーザー名未設定"
+
+    click_on "ログアウト"
+
+    expect(page).to have_content "ログイン"
+  end
 end
